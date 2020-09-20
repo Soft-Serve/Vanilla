8ce1e6dd054b365492d74c72295fb1bba9d208ca
shahyn <shahyn@wiris.com>
2020-09-20 23:41:17 +0200

    upgraded to vue 3

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -12,47 +12,17 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'import/extensions': ['off', 'never'],
-    "no-underscore-dangle": 'off',
-    'class-methods-use-this': 0,
-    'max-len': [
-      'error',
-      {
-        code: 150,
-        ignoreComments: true,
-        ignoreUrls: true,
-      },
-    ],
-    'global-require': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/ban-ts-ignore': 0,
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-multiple-empty-lines': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue'],
-      },
-      webpack: {
-        config: 'vue.config.js',
-      },
-    },
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
       files: [
-        './src/**/__tests__/*.spec.{j,t}s',
-        './src/**/__mock__/*.{j,t}s',
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
-      },
-      rules: {
-        'no-unused-expressions': 0,
       },
     },
   ],
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from 'vuex';
 
-Vue.use(Vuex);
-
-export interface RootState {
-  User: Record<string, string>;
-}
-export default new Vuex.Store<RootState>({});
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+});
