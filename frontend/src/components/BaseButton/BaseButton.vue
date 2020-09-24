2362547b716a4226ec7e3ae0559b49f75009da6c
shahyn <shahyn@wiris.com>
2020-09-24 13:16:47 +0200

    added frontend

---
+++ frontend/src/components/BaseButton/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    :disabled="disabled"
+    :type="type"
+    class="button"
+    v-on="$listeners"
+  >
+    <slot>
+      <span
+        class="button--default"
+        :class="{'button--disabled' : disabled }"
+      >
+        {{ text || 'button' }}
+      </span>
+    </slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import './style.css';
+
+@Component({
+  name: 'BaseButton',
+  components: {
+
+  },
+})
+export default class BaseButton extends Vue {
+    @Prop({ required: false, default: 'button' })
+    readonly type!: string;
+
+    @Prop({ required: false })
+    readonly disabled!: boolean;
+
+    @Prop({ required: false, default: 'button' })
+    readonly text!: string;
+}
+</script>
