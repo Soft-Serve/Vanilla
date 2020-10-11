34f61c20759070e4b5b907738861b5533cb9df0b
kristinemcbride <kristinelmcbride@gmail.com>
2020-10-11 11:40:31 -0400

    finished restaurant interactors and specs

--- api/app/controllers/api/menus_controller.rb
+++ api/app/controllers/api/menus_controller.rb
@@ -1,64 +1,64 @@
 module Api
-    class MenusController < ApplicationController  
-        before_action :fetch, only: %i[show update destroy]
+  class MenusController < ApplicationController
+    before_action :fetch, only: %i[show update destroy]
 
-        def index
-            render json: (collection.filter.map { |menu| serialize(menu) })
-        end
-
-        def create
-            menu = MenuInteractor::Create.new(
-                author: current_user,
-                params: strong_params
-              ).call.unwrap!
-        
-            render json: serialize(menu), status: :created
-        end
-
-        def show
-            render json: serialize(@menu)
-        end
-
-        def update
-            menu = MenuInteractor::Update.new(
-                team: @menu,
-                team_params: strong_params
-            ).call.unwrap!
-        
-            render json: serialize(menu)
-        end
-
-        def destroy
-            MenuInteractor::Delete.new(
-                menu: @menu
-            ).call.unwrap!
-        
-            head :no_content
-        end
-
-        private
-
-        def serialize(menu)
-            MenuPresenter.new(menu).serialize
-        end
-
-        def restaurant
-            @restaurant ||= Restaurant.find_by(id: params[:restaurant_id])
-        end
-
-        def collection
-           MenusCollection.new(restaurant)
-        end
-
-        def fetch
-            @menu = Menu.find(params[:id])
-        end
-
-        def strong_params
-            params.permit(
-            :name,
-            :restaurant_id
-            )
-        end 
+    def index
+      render json: (collection.filter.map { |menu| serialize(menu) })
     end
+
+    def create
+      menu = MenuInteractor::Create.new(
+        author: current_user,
+        params: strong_params
+      ).call.unwrap!
+
+      render json: serialize(menu), status: :created
+    end
+
+    def show
+      render json: serialize(@menu)
+    end
+
+    def update
+      menu = MenuInteractor::Update.new(
+        team: @menu,
+        team_params: strong_params
+      ).call.unwrap!
+
+      render json: serialize(menu)
+    end
+
+    def destroy
+      MenuInteractor::Delete.new(
+        menu: @menu
+      ).call.unwrap!
+
+      head :no_content
+    end
+
+    private
+
+    def serialize(menu)
+      MenuPresenter.new(menu).serialize
+    end
+
+    def restaurant
+      @restaurant ||= Restaurant.find_by(id: params[:restaurant_id])
+    end
+
+    def collection
+      MenusCollection.new(restaurant)
+    end
+
+    def fetch
+      @menu = Menu.find(params[:id])
+    end
+
+    def strong_params
+      params.permit(
+        :name,
+        :restaurant_id
+      )
+    end
+  end
 end
--- api/spec/controllers/api/restaurants_controller_spec.rb
+++ api/spec/controllers/api/restaurants_controller_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Api::RestaurantsController do
   let!(:restaurant) { create(:restaurant) }
   let!(:current_user) { create(:user) }
   let(:currency) { 'CAD' }
-  let(:params) do 
-    { 
-      name: 'Akira Back', 
+  let(:params) do
+    {
+      name: 'Akira Back',
       primary_colour: '#3de443',
       secondary_colour: '#000000',
       currency: currency
@@ -17,19 +17,19 @@ RSpec.describe Api::RestaurantsController do
     JSON.parse(response.body)
   end
 
-  describe "GET #show" do
+  describe 'GET #show' do
     let(:id) { restaurant.id }
- 
+
     before { get :show, params: { id: id }  }
 
-    context 'when id is valid' do 
+    context 'when id is valid' do
       it do
         expect(response).to have_http_status(:ok)
         expect(json['id']).to eq(id)
       end
     end
 
-    context 'when id is invalid' do 
+    context 'when id is invalid' do
       let(:id) { 8989 }
       it do
         expect(response).to have_http_status(:not_found)
@@ -37,23 +37,53 @@ RSpec.describe Api::RestaurantsController do
     end
   end
 
-  describe "POST #create" do
+  describe 'POST #create' do
     before { post :create, params: params }
 
-    context 'when params are valid' do 
+    context 'when params are valid' do
       it do
         expect(response).to have_http_status(:created)
         expect(json['name']).to eq('Akira Back')
       end
     end
 
-    context 'when params are invalid' do 
+    context 'when params are invalid' do
       let(:currency) { 'blabla' }
 
       it do
         expect(response).to have_http_status(:unprocessable_entity)
         expect(json).to include('errors')
       end
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:update_params) { {} }
+    before { patch :update, params: update_params.merge(id: restaurant.id)  }
+
+    context 'when params are valid' do
+      let(:update_params) { { name: 'O&B' } }
+      it do
+        expect(json['name']).to eq('O&B')
+      end
+    end
+
+    context 'when params are invalid' do
+      let(:update_params) { { currency: 'dsgwee' } }
+
+      it do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json).to include('errors')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:destroy_params) { { id: restaurant.id } }
+    before { patch :destroy, params: destroy_params }
+
+    it do
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
