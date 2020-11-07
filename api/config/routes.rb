2d9695eca0ea16525703d3e5c9ff045016aedcd6
kristinemcbride <kristinelmcbride@gmail.com>
2020-11-07 17:05:11 +0100

    fix routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,14 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     resources :restaurants, only: %i[show create update destroy] do
-      resources :menus, only: %i[index show create update destroy] do
-        resources :menu_categories, only: %i[index show create update destroy] do
-          resources :menu_items, only: %i[index show create update destroy] do
-            resources :item_sizes, only: %i[index show create update destroy]
-            resources :dietaries, only: %i[index show update create destroy]
-          end
-        end
-      end
+      resources :dietaries
+      resources :menus, only: %i[index show create update destroy]
+    end
+
+    resources :menus, except: %i[index show create update destroy] do
+      resources :menu_categories, only: %i[index show create update destroy]
+    end
+
+    resources :menu_categories, except: %i[index show create update destroy] do
+      resources :menu_items, only: %i[index show create update destroy]
+    end
+
+    resources :menu_items, except: %i[index show create update destroy] do
+      resources :item_sizes, only: %i[index show create update destroy]
+      resources :dietary_instances, only: %i[index create destroy]
     end
   end
 end
