b764f41e8f14eb681dcb50f48beacd580ad75fbe
shahyn <shahyn@wiris.com>
2020-12-08 21:47:25 +0100

    wip: card component

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -12,3 +12,31 @@ export enum SIZES {
   LARGE = 'large',
   EXTRA_LARGE = 'extra-large'
 }
+
+export enum FLEXJUSTIFY {
+  START = 'justify-start',
+  END = 'justify-end',
+  AROUND = 'justify-around',
+  EVENLY = ' justify-evenly',
+  BETWEEN = 'justify-between',
+  CENTER = 'justify-center',
+}
+
+export enum FLEXALIGN {
+  START = 'items-start',
+  END = 'items-end',
+  CENTER = 'items-center',
+  STRETCH = 'items-evenly',
+  BASELINE = 'items-baseline',
+}
+
+export enum CARDDIRECTION {
+  COLUMN = 'column',
+  ROW = 'row',
+}
+
+export enum CARDSTYLES {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  NAKED ='naked',
+}
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,37 +6,77 @@
         v-for="category in categories.collection"
         :key="category.id"
         :buttonStyle="BUTTONSTYLES.WHITE"
-        @click="handleCategoryChange(category)">
-        {{ category.name }}
+        @click="handleCategoryChange(category)"
+        >
+          {{ category.name }}
       </BaseButton>
       <BaseButton @click="toggleAllergyScreen" :buttonStyle="BUTTONSTYLES.SECONDARY">
         Allergies
       </BaseButton>
     </BaseWrapper>
     <p v-if="loading">loading...</p>
-    <BaseWrapper v-if="items.collection.length">
-      <BaseCard v-for="item in items.filteredCollection" :key="item.id" :data="item"/>
+    <BaseWrapper v-if="items.filteredCollection.length">
+      <AppCard v-for="item in items.filteredCollection" :key="item.id">
+        <template #heading>
+            {{ item.name}}
+        </template>
+        <template #addons>
+          <div class="flex justify-start" v-if="item.dietaries.length">
+            <BaseIcon
+              v-for="dietary in item.dietaries"
+              :key="dietary.id"
+              :name="dietary.allergyNameInLowerCase"
+            />
+          </div>
+        </template>
+        <template #body>
+            <span class="text-sm">{{ item.description }}</span>
+        </template>
+        <template #footer>
+           <div class="card__actions">
+            <span class="card__button-container">
+              <BaseButton :buttonSize="SIZES.EXTRASMALL" @click="decrement">
+                  <span>-</span>
+              </BaseButton>
+            </span>
+            <span class="card__counter">
+              {{ counter }}
+            </span>
+            <span class="card__button-container">
+              <BaseButton :buttonSize="SIZES.EXTRASMALL" @click="increment">
+                  <span>+</span>
+              </BaseButton>
+            </span>
+          </div>
+        </template>
+      </AppCard>
     </BaseWrapper>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
-import { BUTTONSTYLES } from '@/helpers';
+import {
+  defineComponent, ref, watch, onUpdated,
+} from 'vue';
+import { BUTTONSTYLES, SIZES } from '@/helpers';
 import useApi from '@/composables/useApi';
+import useCounter from '@/composables/useCounter';
 import { MutationType } from '@/store/mutations';
+import MenuItem from '@/models/MenuItem';
 import BaseWrapper from '~/BaseWrapper/BaseWrapper.vue';
 import BaseButton from '~/BaseButton/BaseButton.vue';
-import BaseCard from '~/BaseCard/BaseCard.vue';
+import BaseIcon from '~/BaseIcon/BaseIcon.vue';
 import SlideOver from '~/SlideOver/SlideOver.vue';
+import AppCard from '~/AppCard/AppCard.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     BaseWrapper,
     BaseButton,
-    BaseCard,
+    BaseIcon,
     SlideOver,
+    AppCard,
   },
   setup() {
     const {
@@ -49,11 +89,17 @@ export default defineComponent({
       items,
     } = useApi();
 
+    const { counter, increment, decrement } = useCounter();
+
     const isAllergyScreenVisible = ref(false);
 
     const toggleAllergyScreen = () => {
       isAllergyScreenVisible.value = !isAllergyScreenVisible.value;
     };
+
+    onUpdated(() => {
+      items.value.collection.forEach((item: MenuItem) => item.fetchAllergies(item));
+    });
 
     watch(activeDietaries, (selectedDietaries) => {
       const filteredItems = items.value.filterItemsByDietaries(selectedDietaries);
@@ -73,6 +119,10 @@ export default defineComponent({
       toggleAllergyScreen,
       isAllergyScreenVisible,
       activeDietaries,
+      SIZES,
+      counter,
+      increment,
+      decrement,
     };
   },
 
@@ -100,4 +150,66 @@ export default defineComponent({
   box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.5);
   transition: all 0.5s ease-in-out;
 }
+
+.card {
+  @apply flex rounded-lg overflow-hidden shadow-2xl bg-white;
+}
+
+.card__image-container {
+  @apply flex flex-col justify-center ml-2;
+}
+
+.card__button {
+  @apply inline-flex items-center px-2.5 py-1.5 border border-transparent bg-red-600
+    text-xs leading-4 font-bold rounded-full text-white transition ease-in-out duration-150;
+}
+
+.card__button:focus {
+  @apply outline-none border-red-700 shadow-outline-red;
+}
+
+.card__button:hover {
+  @apply bg-red-500;
+}
+
+.card__button:active {
+  @apply bg-red-700;
+}
+
+.card__content {
+  @apply flex-1 bg-white p-4 flex flex-col w-full justify-between items-start flex-wrap;
+}
+
+.card__title {
+  @apply mt-2 leading-7 font-semibold text-gray-900 m-1;
+}
+
+.card__description {
+  @apply mt-2 leading-3 text-gray-500 text-xs italic;
+}
+
+.card__pricing {
+  @apply flex w-full justify-between items-center mt-2;
+}
+
+.card__price {
+  @apply inline-flex w-full text-sm italic;
+}
+
+.card__currency {
+  @apply text-red-500 mx-1;
+}
+
+.card__actions {
+  @apply w-full inline-flex justify-end items-center;
+}
+
+.card__button-container {
+  @apply inline-flex rounded-full shadow-sm mx-2;
+}
+
+.card__counter {
+  @apply font-bold text-sm;
+}
+
 </style>
