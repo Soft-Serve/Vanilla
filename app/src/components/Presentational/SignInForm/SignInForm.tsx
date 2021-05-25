4488bc1d40f80c350e7c952920e498847a32e5c7
shahynKamali <shahynkamali@gmail.com>
2021-05-25 09:14:25 -0400

    added sign in mutation

--- app/src/components/Presentational/SignInForm/SignInForm.tsx
+++ app/src/components/Presentational/SignInForm/SignInForm.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import type { FC } from "react";
 import { Input, Button } from "@base";
+import { useSignInFormMutation } from "./SignInForm.mutation";
 
 const SignInForm: FC = () => {
+  // const [password, setPassword] = useState(" ");
+  // const [email, setEmail] = useState(" ");
+  const [signIn, { data }] = useSignInFormMutation();
+  const [input, setInput] = useState({ email: "", password: "" });
+
+  console.log(data);
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        signIn({
+          variables: {
+            input,
+          },
+        });
+      }}
+    >
       <Input
+        onChange={(e) => setInput({ ...input, email: e.target.value })}
+        value={input.email}
         id="email-address"
         name="email"
         type="email"
@@ -15,6 +33,8 @@ const SignInForm: FC = () => {
       />
 
       <Input
+        value={input.password}
+        onChange={(e) => setInput({ ...input, password: e.target.value })}
         id="password"
         name="password"
         type="password"
