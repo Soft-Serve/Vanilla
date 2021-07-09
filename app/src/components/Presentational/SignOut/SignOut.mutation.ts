961d7af4e0da3f0293bfd4de613ab9ab9f2e9ab7
Kristine McBride <kristinelmcbride@gmail.com>
2021-07-10 00:26:28 +0200

    added stugg

---
+++ app/src/components/Presentational/SignOut/SignOut.mutation.ts
@@ -0,0 +1,32 @@
+import type { MutationHookOptions } from "@apollo/client";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+
+const SIGN_OUT_MUTATION = gql`
+    mutation SignOutMutation($input: input) {
+        signOut(input: $input) @rest(type: SignOut, path: "users/sign_out", method: "DELETE") {
+        }
+    }
+`;
+
+interface User {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    role: string;
+    __typename: string;
+}
+
+interface SignOutData {
+    user: User;
+}
+
+interface Variables {
+    input: Record<string, unknown>;
+}
+
+const useSignOutMutation = (options?: MutationHookOptions<SignOutData, Variables>) =>
+    useMutation<SignOutData, Variables>(SIGN_OUT_MUTATION, options);
+
+export { useSignOutMutation };
