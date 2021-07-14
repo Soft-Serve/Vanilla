053054b8f61699502f4143cb5c7b1d46859f4b19
shahynKamali <shahynkamali@gmail.com>
2021-07-13 20:57:14 -0400

    fixed background

--- app/src/components/Base/BoxSection/styles.ts
+++ app/src/components/Base/BoxSection/styles.ts
@@ -1,7 +1,7 @@
 import { classnames } from "tailwindcss-classnames";
 
 const boxSection = {
-  base: classnames("flex", "flex-col", "w-full", "h-full"),
+  base: classnames("flex", "flex-col", "w-full", "h-auto"),
 };
 
 export { boxSection };
--- app/src/components/Base/Tabs/Tabs.tsx
+++ app/src/components/Base/Tabs/Tabs.tsx
@@ -4,21 +4,6 @@ import type { FC } from "react";
 const Tabs: FC = ({ children }) => {
   return (
     <div>
-      {/* <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-          defaultValue={tabs.find(tab => tab.current)?.name}
-        >
-          {tabs.map(tab => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div> */}
       <div className="sm:block">
         <nav
           className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200 justify-items-center items-center"
