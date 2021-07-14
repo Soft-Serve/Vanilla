8aacf5385cd3a737eb7bf2f15b82a15b68caf055
shahynKamali <shahynkamali@gmail.com>
2021-07-14 18:35:13 -0400

    merged master

--- app/src/components/Presentational/Item/Item.tsx
+++ app/src/components/Presentational/Item/Item.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import type { FC } from "react";
-import { Card, Button, Modal, Pill } from "@base";
+import { Card, Button, Modal } from "@base";
 import { useAllergyContext } from "@contexts";
 import { useDietaryQuery, MenuItem } from "@graphql";
 import intersection from "src/utility/intersection";
@@ -40,10 +40,10 @@ const Item: FC<Props> = ({ item }) => {
     return null;
   }
 
-  const renderUnit = (unit: string) => {
-    if (unit === "1") return null;
-    return unit;
-  };
+  // const renderUnit = (unit: string) => {
+  //   if (unit === "1") return null;
+  //   return unit;
+  // };
 
   const handleClick = () => {
     if (sizes?.itemSizes && sizes.itemSizes.length > 1) {
@@ -62,7 +62,7 @@ const Item: FC<Props> = ({ item }) => {
           <div className="block flex-shrink-0 mr-2">
             <img
               className="w-32 h-20 object-cover rounded-md"
-              src="https://picsum.photos/200"
+              src="https://loremflickr.com/320/240/food"
               alt=""
             />
           </div>
@@ -78,7 +78,7 @@ const Item: FC<Props> = ({ item }) => {
           <ItemSizes itemID={item.id} />
           <Button onClick={handleClick}>{renderButtonText()}</Button>
           <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-            {sizes?.itemSizes.map(size => (
+            {/* {sizes?.itemSizes.map(size => (
               <div className="flex justify-between m-2 p2 items-center" key={size.id}>
                 <span className="text-sm font-extrabold text-red-400 m-2 ">
                   $ {size.price.toFixed(2)}
@@ -86,7 +86,28 @@ const Item: FC<Props> = ({ item }) => {
                 <Pill>{renderUnit(size.unit)}</Pill>
                 <Button>select</Button>
               </div>
-            ))}
+            ))} */}
+
+            <span className="relative z-0 inline-flex shadow-sm rounded-md">
+              <button
+                type="button"
+                className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                Years
+              </button>
+              <button
+                type="button"
+                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                Months
+              </button>
+              <button
+                type="button"
+                className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                Days
+              </button>
+            </span>
           </Modal>
         </div>
       </div>
